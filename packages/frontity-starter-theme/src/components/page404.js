0f8cb92e683f4e3a2b7484da2e956550d370de04
alexadark <alexaspalato@gmail.com>
2020-03-31 09:37:24 +0200

    404

--- packages/frontity-starter-theme/src/components/page404.js
+++ packages/frontity-starter-theme/src/components/page404.js
@@ -1,7 +1,34 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx, Container, Flex, Box } from "theme-ui";
 
-const Page404 = () => {
-  return <h1>Not Found</h1>;
-};
-
+const Page404 = () => (
+  <Container>
+    <Flex
+      sx={{ justifyContent: "center", alignItems: "center", height: "100%" }}
+    >
+      <Box>
+        <h1
+          sx={{
+            p: 20,
+            border: `10px double`,
+            borderColor: "text",
+            textAlign: "center",
+            color: "primary"
+          }}
+        >
+          404
+        </h1>
+        <h2
+          sx={{
+            textTransform: "uppercase",
+            textAlign: "center",
+            color: "secondary"
+          }}
+        >
+          Page Not Found!
+        </h2>
+      </Box>
+    </Flex>
+  </Container>
+);
 export default Page404;
